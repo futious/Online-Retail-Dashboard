1a280826225a7ca5d60a36e2c7e403139270bfc8
futious <49052260+futious@users.noreply.github.com>
2022-01-19 19:16:53 -1000

    Delete setup.bash

--- setup.bash
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/futious/Online-Retail-Dashboard.git
-unzip OnlineRetail.csv.zip
-pip install dash==2.0.0
-pip install pycountry==20.7.3
-pip install dash-bootstrap-components==0.13.1
